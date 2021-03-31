d337c7c656526751582274566567c10deaa5acca
Ivan <inzarubin80@gmail.com>
2021-03-31 13:00:16 +0300

    +

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
  
 import Login from './components/LoginPage/LoginPage'
+import heder from './components/heder/heder'
+
 
 function App() {
+
+ const login = true
   return (
     <div className="btb">
      
-        <Router>
-            <>
+     {!login && <Login/>}
 
+     {login && <heder/>}
+
+     {login && <Router>
+            <>
                 <Switch>
                     <Route path="/" exact component={Login}/>
-                </Switch>
-              
-               
+                </Switch>      
             </>
-        </Router>
+        </Router>}
+
      
     </div>
 )
