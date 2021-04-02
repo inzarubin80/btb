59323ceaf6de75370566e4e27f404e74552f3822
Ivan <inzarubin80@gmail.com>
2021-04-02 17:48:56 +0300

    +

--- src/components/Maket/Makets.js
+++ src/components/Maket/Makets.js
@@ -21,24 +21,16 @@ import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(code, product, chromaticity) {
+  return { code, product, chromaticity};
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('1', "305", "3.7"),
+  createData('2', "305", "3.7"),
+  createData('3', "305", "3.7"),
+  createData('4', "305", "3.7"),
+  
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -68,11 +60,9 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'code', numeric: false, disablePadding: true, label: 'Код' },
+  { id: 'product', numeric: false, disablePadding: false, label: 'Продукт' },
+  { id: 'chromaticity', numeric: false, disablePadding: false, label: 'Цветность'},
 ];
 
 function EnhancedTableHead(props) {
@@ -216,7 +206,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Makets() {
   const classes = useStyles();
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('code');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
@@ -301,6 +291,8 @@ export default function Makets() {
                   const isItemSelected = isSelected(row.name);
                   const labelId = `enhanced-table-checkbox-${index}`;
 
+                  console.log(labelId);
+
                   return (
                     <TableRow
                       hover
@@ -320,16 +312,17 @@ export default function Makets() {
                       <TableCell component="th" id={labelId} scope="row" padding="none">
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                    
+                      <TableCell align="right">{row.code}</TableCell>
+                      <TableCell align="right">{row.product}</TableCell>
+                      <TableCell align="right">{row.chromaticity}</TableCell>
+               
                     </TableRow>
                   );
                 })}
               {emptyRows > 0 && (
                 <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                  <TableCell colSpan={6} />
+                  <TableCell colSpan={1} />
                 </TableRow>
               )}
             </TableBody>
