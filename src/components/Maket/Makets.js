c89eef4489c38292212b0b99b316eeee8500837d
Ivan <inzarubin80@gmail.com>
2021-04-05 17:34:53 +0300

    +

--- src/components/Maket/Makets.js
+++ src/components/Maket/Makets.js
@@ -1,58 +1,112 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-import Button from '@material-ui/core/Button';
+
+import {getMakets} from '../../api/dataService1c';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+import ArchiveIcon from '@material-ui/icons/Archive';
+import HowToRegIcon from '@material-ui/icons/HowToReg';
+import BuildIcon from '@material-ui/icons/Build';
+import { makeStyles } from '@material-ui/core/styles';
+import LocalPrintshopIcon from '@material-ui/icons/LocalPrintshop'
+
+
+import {
+  Link
+} from "react-router-dom";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+
+  stickToBottom: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+  },
+  
+
+}));
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 80, renderCell: (params) => (
+  { field: 'code', headerName: 'Код', width: 80,  type: 'number', renderCell: (params) => (
     <strong>
-     
-      <Button
-        variant="contained"
-        color="primary"
-        size="small"
-        style={{ marginLeft: 1 }}
-
-        onClick={()=>{console.log(params)}}
-      >
-        Open
-      </Button>
+      <Link to={`/makets/${params.value}`}>{params.value}</Link>
     </strong>
   ),},
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
+
+  { field: 'product', headerName: 'Продукт', width: 450, type: 'string' },
+  { field: 'finalBuyer', headerName: 'Конечный покупатель', width: 200, type: 'string' },
+  { field: 'Shell', headerName: 'Вид оболочки', width: 200, type: 'string' },
+  { field: 'shellType', headerName: 'Тип оболочки', width: 200, type: 'string' },
+  { field: 'shellСolor', headerName: 'Цвет оболочки', width: 200, type: 'string' },
+  { field: 'dateAgreement', headerName: 'Дата согласования', width: 200,  type: 'date'},  
+  { field: 'chromaticity', headerName: 'Цветность', width: 130 , type: 'string'},
+  { field: 'caliber', headerName: 'Калибр', width: 130 , type: 'string'},
+  { field: 'typPrinting', headerName: 'Тип печати', width: 130 , type: 'string'},
+
+
+ 
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
 
 export default function Makets() {
+
+  const [maketsAr, setMakets] = React.useState([]);
+
+  const handleChangeBottomNavigation = (event, newStatus) => {
+    setStatus(newStatus);
+  };
+
+  const classes = useStyles();
+
+  
+  const [status, setStatus] = React.useState('harmonization');
+
+  React.useEffect(() => {
+    
+    getMakets(status)
+      .then(response => response.json())
+      .then((json) => {
+        setMakets(json);
+        console.log(json)
+      })
+      .catch((err) => {
+        
+        //setMakets(setEventsFailure());
+
+      });
+  
+  }, [status]);
+
+  //style={{ height: 400, width: '100%' }}
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid rows={rows} columns={columns} pageSize={100}/>
+    <div style={{ height: '800px', width: '100%' }}> 
+    
+      <DataGrid rows={maketsAr} columns={columns} rowHeight={25}/>
+      
+      
+      {/*
+      <BottomNavigation value={status} onChange={handleChangeBottomNavigation} className={classes.stickToBottom} showLabels>
+      <BottomNavigationAction label="Согласование"   value="harmonization" icon={<HowToRegIcon />} />
+      <BottomNavigationAction label="Разработка"     value="development" icon={<BuildIcon />} />
+      <BottomNavigationAction label="Готовые"       value="ready" icon={<LocalPrintshopIcon />} />
+      <BottomNavigationAction label="Архив"         value="archive" icon={<ArchiveIcon />} />
+      </BottomNavigation>
+      */}
+
     </div>
   );
 }
