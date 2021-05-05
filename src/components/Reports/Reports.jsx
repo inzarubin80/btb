d8d44c23acabd11525d966c73ee6a7a1a07f58a3
Zarubin <inzarubin80@gmail.com>
2021-05-05 23:40:00 +0300

    +

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-
-
-import SendIcon from '@material-ui/icons/Send';
-
 import FolderIcon from '@material-ui/icons/Folder';
-
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import AssessmentIcon from '@material-ui/icons/Assessment';
 import Divider from '@material-ui/core/Divider';
-
-
 
 const useStyles = makeStyles((theme) => ({
     root: {
